08f53cd29ae2be5e17a0afefafccaee2356e3986
daniel huynh <danielhuynh@daniels-MacBook-Air-2.local>
2026-03-12 12:27:08 -0700

    Websocket Bug Fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -8,7 +8,7 @@ function sendJson(socket, payload){
 
 function broadcast(wss, payload){
     for (const client of wss.clients){
-        if(client.readyState !== WebSocket.OPEN) return;
+        if(client.readyState !== WebSocket.OPEN) continue;
 
         client.send(JSON.stringify(payload));
     }
@@ -17,12 +17,28 @@ function broadcast(wss, payload){
 export function attachWebSocketServer(server){
     const wss = new WebSocketServer({server, path: '/ws', maxPayload: 1024 * 1024});
 
-    server.on('connection', (socket) => {
+    wss.on('connection', (socket) => {
+        //ping pong test
+        socket.isAlive = true;
+        socket.on('pong', () => {socket.isAlive = true;});
+
         sendJson(socket, { type: 'welcome' });
 
         socket.on('error', console.error)
 
     })
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+        })
+    }, 3000)
+
+    wss.on('close', () => clearInterval(interval));
+
+
 
     function broadcastMatchCreated(match){
         broadcast(wss, {type: 'match_created', data: match});
